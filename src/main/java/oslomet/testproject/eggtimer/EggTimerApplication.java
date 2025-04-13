20e61eefe83990799d65464306e5f10fa563cab2
Andreas <jakan7283@oslomet.no>
2025-04-13 23:39:11 +0200

    Add Spring Boot files

---
+++ src/main/java/oslomet/testproject/eggtimer/EggTimerApplication.java
@@ -0,0 +1,13 @@
+package oslomet.testproject.eggtimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggTimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggTimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomet/testproject/eggtimer/EggTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.testproject.eggtimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggTimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
